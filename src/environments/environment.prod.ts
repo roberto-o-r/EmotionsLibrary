91d0f281d0101523777b792b0f4a62784f0da599
isscroberto <issc.roberto@outlook.com>
2018-03-05 14:31:28 -0600

    Updated environment values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBZMdr-5CS1fdme83u6YmfXIaFWEb8T-Fs',
+    authDomain: 'emotionslibrary.firebaseapp.com',
+    databaseURL: 'https://emotionslibrary.firebaseio.com',
+    projectId: "emotionslibrary",
+    storageBucket: "emotionslibrary.appspot.com",
+    messagingSenderId: "577867813919"
+  }
 };
