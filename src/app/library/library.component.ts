25b2ad8cb880d8785d47184debbac0c28ba7ecab
isscroberto <issc.roberto@outlook.com>
2018-04-20 19:05:04 -0500

    Connect library with firestore

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FeelingService } from '../shared/services/feeling.service';
 
 @Component({
   selector: 'app-library',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryComponent implements OnInit {
 
-  constructor() { }
+  feelings;
+
+  constructor(private feelingService: FeelingService) { }
 
   ngOnInit() {
+    // Get feelings.
+    this.feelingService.getFeelings().subscribe(feelings => {
+      this.feelings = feelings;
+      this.feelings.map(feeling => {
+        const relatedFeelings = feelings.filter(f => feeling.related.some(r => r == f.id));
+        const oppositeFeelings = feelings.filter(f => feeling.opposite.some(r => r == f.id));
+        feeling.relatedFeelings = relatedFeelings;
+        feeling.oppositeFeelings = oppositeFeelings;
+        return { ...feeling }
+      })
+    });
   }
 
 }
